4f5feb4874457b8a92602c827d0dab6edb7f0162
Aditya-kumar-13 <aks13042006@gmail.com>
2024-09-16 14:02:07 +0530

    Todo css updated

--- mern-chat-frontend/src/pages/todo.js
+++ mern-chat-frontend/src/pages/todo.js
@@ -1,85 +1,3 @@
-// import { Row, Col, Title, Typography, Input, Button, Card } from "antd";
-// import { CheckCircleOutlined } from "@ant-design/icons";
-// import io from "socket.io-client";
-// import { Grid } from "antd";
-// import { useEffect, useState } from "react";
-// import Item from "antd/es/list/Item";
-// const Todo = ({ socket }) => {
-// 	const { Title } = Typography;
-// 	const screen = Grid.useBreakpoint();
-// 	const [task, setTask] = useState([]);
-// 	const [taskList, setTaskList] = useState([]);
-
-// 	const addTask = () => {
-// 		socket.emit("addTask", { task });
-// 		setTask("");
-// 	};
-
-// 	useEffect(() => {
-// 		socket.on("allTasks", (tasks) => {
-// 			setTaskList(tasks);
-// 		});
-
-// 		// Clean up when component unmounts
-// 		return () => socket.off("allTasks");
-// 	}, []);
-// 	return (
-// 		<div className="main">
-// 			<div className="App">
-// 				<Col xs={24} md={24}>
-// 					<Row className="Rows">
-// 						<Title level={2} style={{ color: "blue", margin: "1rem" }}>
-// 							Task Manager
-// 						</Title>
-// 					</Row>
-// 					<Row xs={12} md={12} className="Rows" gutter={12}>
-// 						<Col xs={16} md={16} className="Cols">
-// 							<Input
-// 								placeholder="Task"
-// 								onChange={(event) => {
-// 									setTask(event.target.value);
-// 								}}
-// 							/>
-// 						</Col>
-// 						<Col xs={6} md={6} className="Cols">
-// 							<Button
-// 								onClick={addTask}
-// 								style={{
-// 									backgroundColor: "blue",
-// 									height: "2rem",
-// 									borderRadius: "5px",
-// 									width: "100%",
-// 								}}
-// 							>
-// 								Add Task
-// 							</Button>
-// 						</Col>
-// 					</Row>
-// 					<Row className="todo">
-// 						<Col>
-// 							{taskList.map((item) => (
-// 								<Row span={24} key={item._id} gutter={12} justify={"space-between"}>
-// 									<Col>{item.task}</Col>
-
-// 									<Col>
-// 										<Button
-// 											// onClick={() => deleteTask(item._id)}
-// 											style={{ width: "100%" }}
-// 										>
-// 											<CheckCircleOutlined />
-// 										</Button>
-// 									</Col>
-// 								</Row> // Render task as string
-// 							))}
-// 						</Col>
-// 					</Row>
-// 				</Col>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default Todo;
 import React, { useEffect, useState } from "react";
 import { Row, Col, Typography, Input, Button } from "antd";
 import { CheckCircleOutlined } from "@ant-design/icons";
@@ -115,74 +33,85 @@ const Todo = ({ socket }) => {
 	}, [socket]);
 
 	return (
+		
 		<div className="main">
 			<div className="App">
-				<Col xs={24} md={24}>
-					<Row className="Rows">
-						<Title level={2} style={{ color: "#9B59B6", margin: "1rem" }}>
-							Task Manager
-						</Title>
-					</Row>
-					<Row xs={12} md={12} className="Rows" gutter={12}>
-						<Col xs={16} md={16} className="Cols">
-							<Row>
-							<label>Task: </label><br></br>
-								<Input
-								className="search"
-								placeholder="Task"
-								value={task}
-								onChange={(event) => setTask(event.target.value)}
-							/>
-							</Row>
-							<Row>
-								<label>Deadline: </label><br></br>
-							<Input
-							 	type="date"
-								className="search"
-								value={deadline}
-								onChange={(event) => setDeadline(event.target.value)}
-							/>
-							</Row>
-							<Row>
-							<Button
-								onClick={addTask}
-								style={{
-									color: "white",
-									backgroundColor: "#9B59B6",
-									height: "2rem",
-									borderRadius: "5px",
-									width: "100%",
-									border: "none",
-								}}
-							>
-								Add Task
-							</Button>
-							</Row>
-						</Col>
-					</Row>
-					<Row className="todo">
-						<Col className="task-list-container">
-							{" "}
-							{/* Apply scrollable container */}
-							{taskList.map((item) => (
-								<Row span={24} key={item._id} gutter={12} justify={"space-between"}>
-									<Col>{item.task}</Col>
-								
-									<Col>{new Date(item.deadline).toLocaleDateString() }</Col><br></br>
-									<Col>
-										<Button
-											className="complete"
-											onClick={() => deleteTask(item._id)}
-											style={{ width: "100%" }}
-										>
-											<CheckCircleOutlined />
-										</Button>
-									</Col><br></br>
+				{/* Main Row to hold both inputTask and taskList side by side */}
+				<Row gutter={24} className="main-row">
+					<Col className="inputTask" xs={24} md={12}>
+						<Row className="Rows">
+							<Title level={2} style={{ color: "#9B59B6", margin: "1rem" }}>
+								Task Manager
+							</Title>
+						</Row>
+						<Row className="Rows" gutter={12}>
+							<Col xs={24}>
+								<Row>
+									<label>Task:</label><br />
+									<Input
+										className="search"
+										placeholder="Task"
+										value={task}
+										onChange={(event) => setTask(event.target.value)}
+									/>
 								</Row>
-							))}
-						</Col>
-					</Row>
-				</Col>
+								<Row style={{ marginTop: "1rem" }}>
+									<label>Deadline:</label><br />
+									<Input
+										type="date"
+										className="search"
+										value={deadline}
+										onChange={(event) => setDeadline(event.target.value)}
+									/>
+								</Row>
+								<Row style={{ marginTop: "1rem" }}>
+									<Button
+										onClick={addTask}
+										style={{
+											color: "white",
+											backgroundColor: "#9B59B6",
+											height: "2rem",
+											borderRadius: "5px",
+											width: "100%",
+											border: "none",
+										}}
+									>
+										Add Task
+									</Button>
+								</Row>
+							</Col>
+						</Row>
+					</Col>
+
+					{/* Task list column */}
+					<Col className="taskList" xs={24} md={12}>
+						<Row className="todo">
+							<Col className="task-list-container">
+								{taskList.map((item) => (
+									<Row
+										key={item._id}
+										gutter={12}
+										justify={"space-between"}
+										className="task-row"
+										style={{ marginBottom: "1rem" }}
+									>
+										<Col className="task-text">{item.task}</Col>
+										<Col>{new Date(item.deadline).toLocaleDateString()}</Col>
+										<Col>
+											<Button
+												className="complete"
+												onClick={() => deleteTask(item._id)}
+												style={{ width: "100%" }}
+											>
+												<CheckCircleOutlined />
+											</Button>
+										</Col>
+									</Row>
+								))}
+							</Col>
+						</Row>
+					</Col>
+				</Row>
 			</div>
 		</div>
 	);
